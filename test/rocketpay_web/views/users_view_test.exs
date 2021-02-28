04a95f417c33b7b1f36772825ee651eb522dfc2e
Rafael Cerqueira <rafascerqueira.dev@gmail.com>
2021-02-28 15:27:24 -0300

    first commit and publish NLW#4 Elixir stack

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Userteste",
+      password: "123456",
+      nickname: "usertest",
+      email: "usertest@email.com",
+      age: 30
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User Created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Userteste",
+        nickname: "usertest"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
